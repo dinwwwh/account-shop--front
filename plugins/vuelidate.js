7f0016077e179238a10b19ecb91c456a198309ba
dinhdjj <dinhdjj@gmail.com>
2021-06-20 20:54:46 +0700

    build: add some case in ``getValidatorErrorMessage`` method

--- plugins/vuelidate.js
+++ plugins/vuelidate.js
@@ -8,14 +8,44 @@ export default function (context, inject) {
 
 const messages = {
   required: 'là bắt buộc.',
+  minLength: 'quá ngắn, vui lòng bổ sung thêm.',
+  maxLength: 'quá dài, vui lòng rút ngắn lại.',
+  minValue: 'giá trị quá nhỏ, vui lòng tăng thêm.',
+  maxValue: 'giá trị quá lớn, vui lòng giảm xuống.',
+  between: 'có giá trị không hợp lệ',
+  alpha: 'phải là chữ cái, vui lòng loại bỏ các ký tự không hợp lệ.',
+  alphaNum: 'phải là chữ cái hoặc số, vui lòng loại bỏ các ký tự không hợp lệ.',
+  numeric: 'phải là số.',
   integer: 'phải là số nguyên dương.',
+  decimal: 'có giá trị không hợp lệ',
+  email: 'phải là một email hợp lệ.',
+  sameAs: 'có giá trị xác nhận không chính xác.',
 };
 
 function getValidatorErrorMessage(validator, options) {
   if (!validator.$error) return null;
 
   let errorMessage;
-  ['required', 'integer'].some((field) => {
+  const fields = [
+    // Priority group
+    'required',
+    'sameAs',
+    // Advanced group
+    'integer',
+    'alpha',
+    'alphaNum',
+    'numeric',
+    'decimal',
+    'email',
+    // Base group
+    'minLength',
+    'maxLength',
+    'minValue',
+    'maxValue',
+    'between',
+  ];
+
+  fields.some((field) => {
     if (validator[field] === false) {
       errorMessage = `Trường này ${messages[field]}`;
       return true;

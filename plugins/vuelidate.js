161a17529c6e8fa1612428c849eee38960da4e8b
dinhdjj <dinhdjj@gmail.com>
2021-06-20 10:18:48 +0700

    build: add ``this.$getValidatorErrorMessage`` method for get error message of vuelidate

--- plugins/vuelidate.js
+++ plugins/vuelidate.js
@@ -1,3 +1,28 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
 Vue.use(Vuelidate);
+
+export default function (context, inject) {
+  inject('getValidatorErrorMessage', getValidatorErrorMessage);
+}
+
+const messages = {
+  required: 'là bắt buộc.',
+  integer: 'phải là số nguyên dương.',
+};
+
+function getValidatorErrorMessage(validator, options) {
+  if (!validator.$error) return null;
+
+  let errorMessage;
+  ['required', 'integer'].some((field) => {
+    if (validator[field] === false) {
+      errorMessage = `Trường này ${messages[field]}`;
+      return true;
+    }
+
+    return false;
+  });
+
+  return errorMessage;
+}

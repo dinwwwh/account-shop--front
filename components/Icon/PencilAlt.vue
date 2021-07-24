99839c640d33b6d805fcd341a621789fa80fbc9c
dinhdjj <dinhdjj@gmail.com>
2021-07-24 16:42:37 +0700

    build: add some icons

--- components/Icon/PencilAlt.vue
+++ components/Icon/PencilAlt.vue
@@ -1,13 +1,17 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5"
     viewBox="0 0 20 20"
     fill="currentColor"
     v-on="$listeners"
   >
     <path
+      d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+    />
+    <path
       fill-rule="evenodd"
-      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+      d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
       clip-rule="evenodd"
     />
   </svg>

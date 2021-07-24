ffa0a084bbe6a0459114d86990f6adce7d6d084f
dinhdjj <dinhdjj@gmail.com>
2021-07-24 16:42:07 +0700

    build: new Group components
    used to group elements

---
+++ components/Group/Base.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="
+      bg-white
+      shadow
+      sm:rounded-lg sm:overflow-hidden
+      px-4
+      py-6
+      sm:px-6
+      space-y-2
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

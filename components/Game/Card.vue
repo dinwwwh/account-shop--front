1a9a3702faa37eeac9641eb6c12baf5f62732d0f
dinhdjj <dinhdjj@gmail.com>
2021-07-27 16:57:49 +0700

    build: base ``index`` page
    used to show games

---
+++ components/Game/Card.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="pt-[18%] h-full">
+    <div class="flex flex-col bg-white rounded-2xl shadow-xl h-full">
+      <div class="flex-1 relative pt-20 md:pt-16 px-6 pb-8 md:px-8">
+        <div
+          class="
+            absolute
+            top-0
+            left-1/2
+            -translate-x-1/2
+            sm:translate-x-0
+            md:left-auto
+            inline-block
+            rounded-full
+            transform
+            -translate-y-1/2
+            w-1/3
+            pt-[33.3333%]
+            sm:w-1/4 sm:pt-[25%]
+            bg-center bg-cover
+          "
+          :style="`background-image: url(${
+            representativeImage || '/images/errors/404/base.jpg'
+          })`"
+        ></div>
+        <h3 class="text-xl font-medium text-blue-gray-900">
+          {{ game.name }}
+        </h3>
+        <span class="font-normal size-base text-blue-gray-700">
+          ({{ game.publisherName }})
+        </span>
+
+        <p class="mt-4 text-base text-blue-gray-500">
+          {{ game.description }}
+        </p>
+      </div>
+      <div class="p-6 bg-blue-gray-50 rounded-bl-2xl rounded-br-2xl md:px-8">
+        <NuxtLink
+          :to="{
+            name: 'game-id',
+            params: {
+              id: game.id,
+            },
+          }"
+          class="text-base font-medium text-blue-700 hover:text-blue-600"
+        >
+          Xem chi tiết
+          <span aria-hidden="true"> →</span></NuxtLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    game: {
+      type: Array,
+      required: true,
+      validator(game) {
+        return game?.representativeImage?.path;
+      },
+    },
+  },
+  computed: {
+    representativeImage() {
+      return this.game?.representativeImage?.path;
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,70 @@
 <template>
-  <div>
-    <GroupBase>
-      <FileInputBase v-model="value" multiple error>
-        <template #label> Lable </template>
-        <template #description> this is description </template>
-      </FileInputBase>
-    </GroupBase>
+  <div class="py-6 space-y-8">
+    <HeadingBase1> Danh sách game </HeadingBase1>
+
+    <ul class="md:grid-cols-2 xl:grid-cols-3 grid grid-cols-1 gap-x-6 gap-y-8">
+      <li v-for="game in games" :key="game.id">
+        <GameCard :game="game" />
+      </li>
+    </ul>
+
+    <PaginationCenteredNumbers
+      v-if="lastPage > 1"
+      v-model="currentPage"
+      :last-page="lastPage"
+      @change="refreshData()"
+    />
   </div>
 </template>
 
 <script>
+async function fetchData($axios, params = {}, query = {}) {
+  const [{ data: games, meta }] = await Promise.all([
+    $axios.$get(`game`, {
+      params: {
+        _requiredModelRelationships: ['representativeImage'],
+        page: query.page ?? 1,
+      },
+    }),
+  ]);
+
+  return {
+    games,
+    currentPage: meta.current_page,
+    lastPage: meta.last_page,
+  };
+}
+
 export default {
-  layout: 'admin',
-  data() {
+  async asyncData({ $axios, params, query }) {
     return {
-      value: undefined,
+      ...(await fetchData($axios, params, query)),
     };
+  },
+  methods: {
+    refreshData() {
+      const query = {
+        page: this.currentPage,
+      };
+
+      this.$nuxt.$loading.start();
+      fetchData(this.$axios, this.$route.params, query)
+        .then(({ games, currentPage, lastPage }) => {
+          this.$nuxt.$loading.finish();
+          this.games = games;
+          this.currentPage = currentPage;
+          this.lastPage = lastPage;
+
+          this.$router.push({
+            name: this.$route.name,
+            params: this.$route.params,
+            query,
+          });
+        })
+        .catch(() => {
+          this.$nuxt.$loading.fail();
+        });
+    },
   },
 };
 </script>

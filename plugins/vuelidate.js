165c154126d643722776935c1c69679ff0f9ff9e
dinhdjj <dinhdjj@gmail.com>
2021-07-28 16:32:12 +0700

    build: upgrate ``vuelidate`` plugin

--- plugins/vuelidate.js
+++ plugins/vuelidate.js
@@ -21,7 +21,7 @@ import {
 } from 'vuelidate/lib/validators';
 
 Vue.use(Vuelidate);
-const validators = {
+const rules = {
   required,
   minLength,
   maxLength,
@@ -43,40 +43,44 @@ const validators = {
   },
 };
 const messages = {
-  required: 'là bắt buộc.',
-  minLength: 'quá ngắn, vui lòng bổ sung thêm.',
-  maxLength: 'quá dài, vui lòng rút ngắn lại.',
-  minValue: 'giá trị quá nhỏ, vui lòng tăng thêm.',
-  maxValue: 'giá trị quá lớn, vui lòng giảm xuống.',
-  between: 'có giá trị không hợp lệ',
-  alpha: 'phải là chữ cái, vui lòng loại bỏ các ký tự không hợp lệ.',
-  alphaNum: 'phải là chữ cái hoặc số, vui lòng loại bỏ các ký tự không hợp lệ.',
-  numeric: 'phải là số.',
-  integer: 'phải là số nguyên dương.',
-  decimal: 'có giá trị không hợp lệ',
-  email: 'phải là một email hợp lệ.',
-  sameAs: 'có giá trị xác nhận không chính xác.',
-  ipAddress: 'phải là một địa chỉ IP hợp lệ.',
-  macAddress: 'phải là một đại chỉ MAC hợp lệ.',
-  url: 'phải là một địa chỉ URL hợp lệ.',
-  in: 'có giá trị không hợp lệ',
+  required: ':name là bắt buộc.',
+  minLength: ':name quá ngắn, vui lòng bổ sung thêm.',
+  maxLength: ':name quá dài, vui lòng rút ngắn lại.',
+  minValue: ':name giá trị quá nhỏ, vui lòng tăng thêm.',
+  maxValue: ':name giá trị quá lớn, vui lòng giảm xuống.',
+  between: ':name có giá trị không hợp lệ',
+  alpha: ':name phải là chữ cái, vui lòng loại bỏ các ký tự không hợp lệ.',
+  alphaNum:
+    ':name phải là chữ cái hoặc số, vui lòng loại bỏ các ký tự không hợp lệ.',
+  numeric: ':name phải là số.',
+  integer: ':name phải là số nguyên dương.',
+  decimal: ':name có giá trị không hợp lệ',
+  email: ':name phải là một email hợp lệ.',
+  sameAs: ':name có giá trị xác nhận không chính xác.',
+  ipAddress: ':name phải là một địa chỉ IP hợp lệ.',
+  macAddress: ':name phải là một đại chỉ MAC hợp lệ.',
+  url: ':name phải là một địa chỉ URL hợp lệ.',
+  in: ':name có giá trị không hợp lệ',
 };
 
 export default function (context, inject) {
   inject('getValidatorErrorMessage', getValidatorErrorMessage);
+  inject('rules', rules);
   inject('vuelidate', {
-    validators,
+    validators: rules,
   });
 }
 
-function getValidatorErrorMessage(validator, options) {
+function getValidatorErrorMessage(validator, name = 'trường này') {
   if (!validator.$error) return null;
 
   let errorMessage;
 
-  Object.keys(validators).some((field) => {
+  Object.keys(rules).some((field) => {
     if (validator[field] === false) {
-      errorMessage = `Trường này ${messages[field]}`;
+      errorMessage = messages[field]?.replaceAll(':name', name);
+      errorMessage =
+        errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
       return true;
     }
 

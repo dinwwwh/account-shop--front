cb23e7e89d446d2626a992dae048c79a2800ae05
dinhdjj <dinhdjj@gmail.com>
2021-07-28 19:32:49 +0700

    feat: realize ``register`` page

---
+++ components/Auth/RegisterForm.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="space-y-4">
+    <!-- Email -->
+    <InputBaseValidatable
+      v-model="authInfos.name"
+      name="tên"
+      placeholder="Lê Định Pro"
+      :rules="{
+        required: $rules.required,
+      }"
+      @emit-validator="validators[0] = $event"
+    >
+      <template #label> Tên </template>
+    </InputBaseValidatable>
+
+    <!-- Email -->
+    <InputBaseValidatable
+      v-model="authInfos.email"
+      type="email"
+      name="email"
+      placeholder="example@mail.com"
+      :rules="{
+        required: $rules.required,
+        email: $rules.email,
+      }"
+      @emit-validator="validators[1] = $event"
+    >
+      <template #label> Email </template>
+    </InputBaseValidatable>
+
+    <!-- Password -->
+    <InputBaseValidatable
+      v-model="authInfos.password"
+      type="password"
+      name="mật khẩu"
+      placeholder="*****"
+      :rules="{
+        required: $rules.required,
+      }"
+      @emit-validator="validators[2] = $event"
+    >
+      <template #label> Mật khẩu </template>
+    </InputBaseValidatable>
+
+    <!-- Password confirm -->
+    <InputBaseValidatable
+      v-model="authInfos.passwordConfirmation"
+      type="password"
+      name="mật khẩu xác nhận"
+      placeholder="*****"
+      :rules="{
+        required: $rules.required,
+        equal: $rules.equal(authInfos.password),
+      }"
+      @emit-validator="validators[3] = $event"
+    >
+      <template #label> Xác nhận mật khẩu </template>
+    </InputBaseValidatable>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'model',
+    event: 'change',
+  },
+  props: {
+    model: {
+      type: Object,
+      default() {
+        const val = {};
+        this.$emit('change', val);
+        return val;
+      },
+    },
+  },
+  data() {
+    return {
+      validators: [],
+    };
+  },
+  computed: {
+    authInfos: {
+      get() {
+        return this.model;
+      },
+      set(val) {
+        this.$emit('change', val);
+      },
+    },
+    panel() {
+      const panel = {
+        validators: this.validators,
+        check() {
+          this.validators.forEach((validator) => validator.vuelidate.$touch());
+          for (const validator of this.validators) {
+            if (!(validator.check() === true)) {
+              return validator.check();
+            }
+          }
+
+          return true;
+        },
+      };
+
+      this.$emit('emit-panel', panel);
+      return panel;
+    },
+  },
+  created() {
+    return this.panel;
+  },
+};
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,107 @@
+<template>
+  <div
+    class="
+      min-h-screen
+      bg-gray-50
+      flex flex-col
+      justify-center
+      py-12
+      sm:px-6
+      lg:px-8
+    "
+  >
+    <!-- Logo -->
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img
+        class="mx-auto h-12 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+        alt="Workflow"
+      />
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Đăng ký
+      </h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Hoặc
+        <NuxtLink
+          :to="{ name: 'login' }"
+          class="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          đăng nhập
+        </NuxtLink>
+      </p>
+    </div>
+
+    <!-- Form -->
+    <div
+      class="
+        bg-white
+        py-8
+        px-4
+        shadow
+        sm:rounded-lg sm:px-10
+        mt-8
+        sm:mx-auto sm:w-full sm:max-w-md
+        space-y-6
+      "
+    >
+      <AuthRegisterForm
+        v-model="authInfos"
+        @emit-panel="registerFormPanel = $event"
+      />
+
+      <!-- actions -->
+      <div class="space-y-4">
+        <MessageBase :message="message" />
+        <ButtonPrimary :loading="loading" @click="register">
+          Đăng ký
+        </ButtonPrimary>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'guest',
+  data() {
+    return {
+      authInfos: {},
+      registerFormPanel: undefined,
+      message: {
+        error: undefined,
+        warn: undefined,
+        success: undefined,
+      },
+      loading: false,
+    };
+  },
+  methods: {
+    register() {
+      this.message.error = null;
+      this.message.warn = null;
+      this.message.success = null;
+
+      if (!this.registerFormPanel.check()) {
+        return;
+      }
+
+      this.loading = true;
+      this.$axios
+        .$post('register', {
+          name: this.authInfos.name,
+          email: this.authInfos.email,
+          password: this.authInfos.password,
+        })
+        .then(() => {
+          this.message.success = 'Bạn có thể đăng nhập ngay.';
+        })
+        .catch(() => {
+          this.message.error = 'Thất bại vui lòng thử lại sau.';
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>

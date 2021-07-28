4e7f8343c21470735160270c093504ca964c36ce
dinhdjj <dinhdjj@gmail.com>
2021-07-28 09:57:38 +0700

    refactor: reduce redundant code

--- plugins/axios.js
+++ plugins/axios.js
@@ -17,16 +17,8 @@ export default function ({ $axios, redirect, error, $typeCheck }, inject) {
   });
 
   function withFile(data, formData = new FormData(), initKey = null) {
-    // Advanced data please toss it higher than instanceof Object
-
-    // Case data is a file
-    if ($typeCheck('File', data)) {
-      formData.append(initKey, data, data.name);
-      return formData;
-    }
-
-    // Case: data is an object
-    else if ($typeCheck('Object', data)) {
+    // Case: data is an object or list
+    if ($typeCheck('Object|Array|FileList', data)) {
       Object.keys(data).forEach((key) => {
         if (initKey) {
           formData = withFile(data[key], formData, `${initKey}[${key}]`);
@@ -36,38 +28,14 @@ export default function ({ $axios, redirect, error, $typeCheck }, inject) {
       });
     }
 
-    // Case: data is an Array
-    else if ($typeCheck('Array|FileList', data)) {
-      // Case must has initKey
-      if (!initKey) {
-        // eslint-disable-next-line no-console
-        console.error('$axios.withFile() first argument must be an object!');
-        return formData;
-      }
-      data.forEach((val, key) => {
-        formData = withFile(val, formData, initKey + `[${key}]`);
-      });
-    }
-
-    // Case: data is an String or Number
-    else if ($typeCheck('String | Number', data)) {
-      formData.append(initKey, data);
-    }
-
     // Case: data is Boolean type
     else if ($typeCheck('Boolean', data)) {
       formData.append(initKey, data ? 1 : 0);
     }
 
-    // Case: data is undefined
-    else if ($typeCheck('Undefined', data)) {
-      return formData;
-    }
-
-    // Case: the rest case
-    else {
+    // Case: data isn't undefined
+    else if (!$typeCheck('Undefined', data)) {
       formData.append(initKey, data);
-      return formData;
     }
 
     return formData;

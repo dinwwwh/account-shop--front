78fab8da7a1b3b2c880a073953967a69c59b3820
dinhdjj <dinhdjj@gmail.com>
2021-07-29 20:19:45 +0700

    style: new ``MessageSimple`` component
    it does not have bg color and padding

---
+++ components/Message/Simple/Info.vue
@@ -0,0 +1,31 @@
+<template>
+  <transition
+    enter-active-class="transition duration-200 ease-out"
+    enter-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition duration-150 ease-in"
+    leave-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div
+      class="
+        flex
+        items-center
+        gap-1
+        text-blue-600
+        rounded-md
+        font-medium
+        relative
+      "
+    >
+      <div><IconInformationCircle class="size-xl" /></div>
+      <div>
+        <slot />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ components/Message/Simple/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="space-y-3">
+    <MessageSimpleSuccess v-if="message.success">
+      {{ message.success }}
+    </MessageSimpleSuccess>
+
+    <MessageSimpleInfo v-if="message.info">
+      {{ message.info }}
+    </MessageSimpleInfo>
+
+    <MessageSimpleWarning v-if="message.warning">
+      {{ message.warning }}
+    </MessageSimpleWarning>
+
+    <MessageSimpleError v-if="message.error">
+      {{ message.error }}
+    </MessageSimpleError>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>

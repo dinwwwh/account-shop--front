8e5f11ad7c14d6715b05fed11848f249af3217ef
dinhdjj <dinhdjj@gmail.com>
2021-07-31 14:45:02 +0700

    style: reduce redundant of some pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,8 @@
 <template>
-  <div
-    class="
-      min-h-screen
-      bg-gray-50
-      flex flex-col
-      justify-center
-      py-12
-      sm:px-6
-      lg:px-8
-    "
-  >
-    <!-- Logo -->
+  <div class="bg-gray-50 flex flex-col justify-center py-6 sm:px-6 lg:px-8">
+    <!-- Title -->
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img
-        class="mx-auto h-12 w-auto"
-        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-        alt="Workflow"
-      />
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      <h2 class="text-center text-3xl font-extrabold text-gray-900">
         Đăng nhập
       </h2>
       <p class="mt-2 text-center text-sm text-gray-600">

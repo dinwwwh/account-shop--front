c87608f080a3772ee9d206f543450aed96e9b6d1
dinhdjj <dinhdjj@gmail.com>
2021-08-01 10:14:01 +0700

    style: change padding of ``GroupSepared component``

--- components/Group/Separated.vue
+++ components/Group/Separated.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-white rounded-sm sm:rounded-md shadow-xl">
-    <div class="flex-1 relative px-4 py-6 md:px-6">
+    <div class="flex-1 relative px-3 py-5 md:px-5">
       <slot />
     </div>
-    <div class="p-4 bg-blue-gray-50 rounded-bl-2xl rounded-br-2xl md:px-8">
+    <div
+      class="px-3 py-2 md:px-5 bg-blue-gray-50 rounded-bl-2xl rounded-br-2xl"
+    >
       <slot name="bottom" />
     </div>
   </div>
